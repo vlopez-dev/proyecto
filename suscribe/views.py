9eeeaca0323fcd4bc7175d96f79b93dc98bfc746
vlopez <victorl_222@hotmail.com>
2021-10-27 17:54:30 -0300

    add broker

--- suscribe/views.py
+++ suscribe/views.py
@@ -17,7 +17,7 @@ import paho.mqtt.client as mqttClient
 def suscripcion_agregar(request):
     if request.method=="GET":
         form = SuscribeForm()
-    
+
         return render(request,'suscribe/agregar.html',{'form':form})
 
     else:
@@ -26,35 +26,10 @@ def suscripcion_agregar(request):
             # ruta=form.cleaned_data.get("ruta","id_suscribe")
             # print(ruta)
             # subscribing(ruta="esp/dht/temperature",id_suscribe=1)
-            
+
             form.save()
-            
 
         return redirect('/suscribe/home.html')
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
 
 
 
@@ -67,18 +42,9 @@ def on_message(client, userdata, message):
     for i in ob:
         id_suscribe=i.id_suscribe
         print(id_suscribe)
-    
-    
-    
-    
-    
-    
-    
     ob= Lectura.objects.create(suscribe_id=id_suscribe,lectura_sensor=mensaje)
     print("creo objecto para guardar lectura")
-    # ob.lectura_sensor= mensaje
     ob.save()
-    # Suscribe.objects.create(mensaje)    
     time.sleep(1)
 
 
@@ -91,8 +57,8 @@ Connected = False   #Variable golabal de conexion
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Conectado al broker")
-        global Connected                
-        Connected = True               
+        global Connected
+        Connected = True
         # subscribing()
     else:
         print("Fallo la conexion") 
@@ -106,11 +72,11 @@ def on_connect(client, userdata, flags, rc):
 def conexion_broker():
     Connected = False   #global variable for the state of the connection
 print("Contado al broker")
-broker_address="inversoft.ddns.net"
-# objeto = Conexion.objects.last()
-# broker=objeto.broker_conexion
-# print(broker)
-# broker_address= broker
+# broker_address="inversoft.ddns.net"
+objeto = Cliente.objects.last()
+broker=objeto.broker_conexion
+print(broker)
+broker_address= broker
 #direccion AP broker "10.3.141.1"
 port = 1883                         #Broker port
 user = "proyecto"                    #Connection username
@@ -127,21 +93,18 @@ print("ejecute el loop de conexion")
 
 
 
-    
-    
-    
-       
+
 def subscribing():
     ruta=""
     while Connected != True:    #Wait for connection
         time.sleep(1)
         
         ob = Suscribe.objects.all() #Busco todos los objetos en la tabla suscribe y los recorro uno suscribiendome al sensor
-        for i in ob:                        
+        for i in ob:
             ruta=i.ruta
             if ruta =="":
                 pass
-            
+
             else:
                 client.subscribe(ruta) 
     #client.subscribe("esp/dht/temperature")  #Linea de suscricion original
